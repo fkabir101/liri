35c63ca25aa9a0970f7202c0c765b470d53a7cae
Fahim Kabir <fkabir101@gmail.com>
2019-01-25 14:56:32 -0500

    spotify this song finished

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const keys = require("./keys.js");
-var axios = require("axios");
-//const spotify = new Spotify(keys.spotify);
+let axios = require("axios");
+var Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
 
 let inputString = process.argv;
 let command = process.argv[2];
@@ -9,13 +10,18 @@ let search = "";
 
 for(let i = 3; i<inputString.length; i++){
   if (i > 3 && i < inputString.length) {
-    search = search + "+" + inputString[i];
+    if(command === "movie-this"){
+      search = search + "+" + inputString[i];
+    }
+    else{
+      search = search + " " + inputString[i];
+    }
   }
   else {
     search += inputString[i];
   }
 }
-let queryUrl = ""
+let queryUrl = "";
 if(command === "movie-this"){
   queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
   axios.get(queryUrl).then(
@@ -32,5 +38,28 @@ if(command === "movie-this"){
       console.log(`Cast: ${data.Actors}`);
       console.log("===================================");
     }
-  );
+  )
+}
+
+else if(command === "spotify-this-song"){
+  spotify.search({ type: 'track', query: search, limit: 1 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  let spotifyData = data.tracks.items[0];
+  let artists = "";
+  for(let i = 0; i < spotifyData.artists.length; i++){
+    if(i !== 0)
+      artists = artists + ", " + spotifyData.artists[i].name;
+    else
+      artists = spotifyData.artists[i].name;
+  }
+  //console.log(spotifyData.album);
+  console.log("============================");
+  console.log(`Artist: ${artists}`);
+  console.log(`Song Name: ${spotifyData.name}`);
+  console.log(`Preview: ${spotifyData.preview_url}`);
+  console.log(`Album: ${spotifyData.name}`);
+  console.log("============================");
+  });
 }
