158ec1ee05ef073cf53ddc5f81a2b9479adeb448
Fahim Kabir <fkabir101@gmail.com>
2019-01-27 18:51:26 -0500

    finished

--- liri.js
+++ liri.js
@@ -1,34 +1,65 @@
+// import all node packages
 require("dotenv").config();
 const keys = require("./keys.js");
 const axios = require("axios");
 const moment = require("moment");
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
+const fs = require("fs");
 
+// declare variables
 let inputString = process.argv;
 let command = process.argv[2];
 let search = "";
 
-for(let i = 3; i<inputString.length; i++){
-  if (i > 3 && i < inputString.length) {
-    if(command === "movie-this"){
-      search = search + "+" + inputString[i];
+if(process.argv[3] !== undefined){
+  for(let i = 3; i<inputString.length; i++){
+    if (i > 3 && i < inputString.length) {
+      if(command === "movie-this"){
+        search = search + "+" + inputString[i];
+      }
+      else if(command === "band-this"){
+        search = search + "%20" + inputString[i];
+      }
+      else{
+        search = search + " " + inputString[i];
+      }
     }
-    else if(command === "band-this"){
-      search = search + "%20" + inputString[i];
+    else {
+      search += inputString[i];
     }
-    else{
-      search = search + " " + inputString[i];
-    }
+
   }
-  else {
-    search += inputString[i];
+  checkCommand();
+}
+else{
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    const dataArr = data.split(",");
+    command = dataArr[0];
+    search = dataArr[1];
+    checkCommand();
+  });
+}
+
+// run this function to check what command was used and run the corresponding function
+function checkCommand(){
+  if(command === "movie-this"){
+    movieThis(search);
+  }
+  else if(command === "concert-this"){
+    concertThis(search);
   }
 
+  else if(command === "spotify-this-song"){
+    spotifyThis(search);
+  }
 }
-let queryUrl = "";
-if(command === "movie-this"){
-  queryUrl = `http://www.omdbapi.com/?t=${search}&y=&plot=short&apikey=trilogy`;
+// function to run when "movie-this command is used"
+function movieThis(search){
+  let queryUrl = `http://www.omdbapi.com/?t=${search}&y=&plot=short&apikey=trilogy`;
   axios.get(queryUrl).then(
     function(response) {
       const data = response.data;
@@ -45,8 +76,9 @@ if(command === "movie-this"){
     }
   );
 }
-else if(command === "concert-this"){
-  queryUrl = `https://rest.bandsintown.com/artists/${search}/events?app_id=codingbootcamp`;
+// function to run when "concert-this command is used"
+function concertThis(search){
+  let queryUrl = `https://rest.bandsintown.com/artists/${search}/events?app_id=codingbootcamp`;
   axios.get(queryUrl).then(
     function(response) {
       const data = response.data[0];
@@ -60,8 +92,8 @@ else if(command === "concert-this"){
     }
   );
 }
-
-else if(command === "spotify-this-song"){
+// function to run with spotify-this-command is used
+function spotifyThis(search){
   spotify.search({ type: 'track', query: search, limit: 1 }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
